23a16aa5a49cd9b8c7a86ec3db053cd266696ded
ISaad <isma.saad@gmail.com>
2025-09-29 17:16:20 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+});
