f548daaa919c82f024fca28f4208d2fa6935fbe8
ISaad <isma.saad@gmail.com>
2025-09-29 17:15:54 +0200

    Create install.js

---
+++ install.js
@@ -0,0 +1,16 @@
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('install-btn').style.display = 'block';
+});
+
+document.getElementById('install-btn').addEventListener('click', () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      deferredPrompt = null;
+      document.getElementById('install-btn').style.display = 'none';
+    });
+  }
+});
